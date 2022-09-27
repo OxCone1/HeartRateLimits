abb5821db25cad8ab586e58daef10295e30bd52f
Vadims Lapins <t1lava01@students.oamk.fi>
2022-09-28 01:40:00 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [userAge, setUserImputAge] = useState(0);
+  const [userCalcLHr, setUserLHr] = useState(0);
+  const [userCalcUHr, setUserUHr] = useState(0);
+  
+  function caclculateHeartRate() {
+    setUserLHr((220 - userAge) * 0.65);
+    setUserUHr((220 - userAge) * 0.85);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='container'>
+      <h2>Heart rate limits</h2>
+      <div>
+        <form>
+          <div>
+            <label> Input your age</label>
+            <input type='number' value={userAge} onChange={(e) => setUserImputAge(e.target.value)} onKeyPress={(e) => { if (e.key === 'Enter') caclculateHeartRate() }} />
+          </div>
+          <div>
+            <label> Heart rate limits</label>
+            <output>{userCalcLHr} - {userCalcUHr}</output>
+          </div>
+          <button type='button' onClick={caclculateHeartRate}>Calculate</button>
+        </form>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
